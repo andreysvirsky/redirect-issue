a079b43c2052c8940be121e6c35657a5ed6fe281
Andrey Svirsky <78034184+andreysvirsky@users.noreply.github.com>
2022-12-06 16:47:37 +0300

    remove comments

--- src/pages/ProtectedPage.tsx
+++ src/pages/ProtectedPage.tsx
@@ -4,8 +4,5 @@ import { useAuthUser } from "../auth/useAuthUser";
 export const ProtectedPage = () => {
   const data = useAuthUser();
 
-  // console.log({ user });
-  // console.log({ username });
-
   return <div>ProtectedPage</div>;
 };
