734c56013ae3c0a9df372c651b4a5552a7afa032
Andrey Svirsky <78034184+andreysvirsky@users.noreply.github.com>
2022-12-08 17:46:50 +0300

    updated WithAuth0 component

--- src/auth/WithAuth0.tsx
+++ src/auth/WithAuth0.tsx
@@ -1,33 +1,24 @@
 import { Auth0Provider } from "@auth0/auth0-react";
 import { useNavigate } from "react-router-dom";
 import type { PropsWithChildren } from "react";
-import type { AppState, Auth0ProviderOptions } from "@auth0/auth0-react";
+import type { AppState } from "@auth0/auth0-react";
 
 export const WithAuth0 = ({ children }: PropsWithChildren) => {
-  const Auth0ProviderWithRedirectCallback = ({
-    children,
-    ...props
-  }: PropsWithChildren<Auth0ProviderOptions>) => {
-    const navigate = useNavigate();
-    const onRedirectCallback = (appState?: AppState) => {
-      navigate((appState && appState.returnTo) || window.location.pathname);
-    };
-
-    return (
-      <Auth0Provider onRedirectCallback={onRedirectCallback} {...props}>
-        {children}
-      </Auth0Provider>
-    );
+  const navigate = useNavigate();
+  const onRedirectCallback = (appState?: AppState) => {
+    navigate(appState?.returnTo || window.location.pathname);
   };
 
   return (
-    <Auth0ProviderWithRedirectCallback
+    <Auth0Provider
       domain={import.meta.env.VITE_AUTH_DOMAIN}
       clientId={import.meta.env.VITE_CLIENT_ID}
       redirectUri={window.location.origin}
       audience={import.meta.env.VITE_AUTH_API_IDENTIFIER}
+      onRedirectCallback={onRedirectCallback}
+      useRefreshTokens
     >
       {children}
-    </Auth0ProviderWithRedirectCallback>
+    </Auth0Provider>
   );
 };
--- src/auth/useAuthUser.ts
+++ src/auth/useAuthUser.ts
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { useAuth0 } from "@auth0/auth0-react";
-import { fetchMockData } from "../helpers/fetchMockData";
+// import { fetchMockData } from "../helpers/fetchMockData";
 
 type StateType = {
   isAuthUserLoading: boolean;
@@ -18,49 +18,38 @@ export function useAuthUser() {
   const [{ isAuthUserLoading, authUserError, authUserName }, setAuthUserState] =
     useState<StateType>(initialState);
   const {
-    user,
     getAccessTokenSilently,
     isLoading: isAuth0Loading,
     error: auth0Error,
     logout,
   } = useAuth0();
-  const userId = user?.sub;
-  const sessionToken = localStorage.getItem("session");
 
-  const getUserData = async () => {
-    try {
-      setAuthUserState((prevState) => ({
-        ...prevState,
-        isAuthUserLoading: true,
-      }));
-      const { username } = await fetchMockData();
-      setAuthUserState({
-        isAuthUserLoading: false,
-        authUserError: null,
-        authUserName: username,
-      });
-    } catch (error) {
-      setAuthUserState({
-        isAuthUserLoading: false,
-        authUserError: error as Error,
-        authUserName: null,
-      });
-    }
-  };
+  // const getUserData = async () => {
+  //   try {
+  //     setAuthUserState((prevState) => ({
+  //       ...prevState,
+  //       isAuthUserLoading: true,
+  //     }));
+  //     const { username } = await fetchMockData();
+  //     setAuthUserState({
+  //       isAuthUserLoading: false,
+  //       authUserError: null,
+  //       authUserName: username,
+  //     });
+  //   } catch (error) {
+  //     setAuthUserState({
+  //       isAuthUserLoading: false,
+  //       authUserError: error as Error,
+  //       authUserName: null,
+  //     });
+  //   }
+  // };
 
   useEffect(() => {
-    if (!sessionToken) {
-      getAccessTokenSilently().then((auth0Token) =>
-        localStorage.setItem("session", auth0Token)
-      );
-    }
-  }, [sessionToken]);
-
-  useEffect(() => {
-    if (userId && !isAuthUserLoading && sessionToken) {
-      getUserData();
-    }
-  }, [userId, sessionToken]);
+    getAccessTokenSilently({ detailedResponse: true }).then((response) => {
+      console.log({ response });
+    });
+  }, []);
 
   return {
     isUserLoading: isAuth0Loading || isAuthUserLoading,
